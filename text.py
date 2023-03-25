26fb69093c33771c171911fafb6a5a4e6ce98346
darkcoiner <darkcoiner@kasescenarios.com>
2023-03-25 13:16:06 +0100

    Update text.py

--- text.py
+++ text.py
@@ -1,3 +1,3 @@
 import pywhatkit as kit
 
-kit.text_to_handwriting("You are running out of time. Send £1,000,000 GBP to 1Doho93x24sbs4jyPw6wn87xjQSuFS8wrt immediately if you want to see your daughter ever again.")
+kit.text_to_handwriting("")
